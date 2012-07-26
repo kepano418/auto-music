b3bf636495cb1a49a3a2f53c5e90afebf2d34d81
jess.louderback@gmail.com <jess.louderback@gmail.com@7c3e6dd4-ce57-9d87-5feb-09a7e010751d>
2012-07-26 00:57:14 +0000

    enabled service start on boot option

--- AutoMusic/src/net/kepano/automusic/AutoMusicActivity.java
+++ AutoMusic/src/net/kepano/automusic/AutoMusicActivity.java
@@ -1,41 +1,49 @@
 package net.kepano.automusic;
 
+import net.kepano.database.DBAdapter;
+import net.kepano.database.DataHandler;
 import service.detectJack;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.app.ActivityManager.RunningServiceInfo;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.ToggleButton;
 
 public class AutoMusicActivity extends Activity {
-	static ToggleButton tb;
+
+	private ToggleButton tb;
+	private CheckBox cb;
+
+	private DBAdapter database;
 
 	/** Called when the activity is first created. */
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		// startService(new Intent(this, detectJack.class));
 		setContentView(R.layout.main);
 
-		tb = (ToggleButton) findViewById(R.id.serviceToggle);
-		tb.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				serviceControl(tb.isChecked());
-			}
-		});
+		database = new DBAdapter(this);
+		database.open();
 
-		tb.setChecked(isServiceRunning());
+		initToggleBox();
+		initCheckBox();
+
 	}
 
 	@Override
 	protected void onDestroy() {
-		//stopService(new Intent(this, service.detectJack.class));
+		// stopService(new Intent(this, service.detectJack.class));
+		database.close();
 		super.onDestroy();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		tb.setChecked(isServiceRunning());
@@ -63,5 +71,41 @@ public class AutoMusicActivity extends Activity {
 			// new state is off and we want it on
 			stopService(new Intent(this, detectJack.class));
 		}
+	}
+
+	private void initToggleBox() {
+		tb = (ToggleButton) findViewById(R.id.serviceToggle);
+		tb.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				serviceControl(tb.isChecked());
+			}
+		});
+		tb.setChecked(isServiceRunning());
+	}
+
+	private void initCheckBox() {
+		cb = (CheckBox) findViewById(R.id.checkBox_OnBoot);
+
+		Cursor c = database.getOption(DataHandler.OPTION_START_ON_BOOT);
+		c.moveToFirst();
+		if (c.getCount() == 1
+				&& c.getString(c.getColumnIndex(DataHandler.TABLE_COL_VALUE))
+						.equals("true"))
+			cb.setChecked(true);
+
+		cb.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(CompoundButton buttonView,
+					boolean isChecked) {
+				if (isChecked) {
+					database.updateOption(DataHandler.OPTION_START_ON_BOOT,
+							"true");
+				} else {
+					database.updateOption(DataHandler.OPTION_START_ON_BOOT,
+							"false");
+				}
+
+			}
+		});
+
 	}
 }
--- AutoMusic/src/service/intentBroadcast.java
+++ AutoMusic/src/service/intentBroadcast.java
@@ -1,27 +1,41 @@
 package service;
 
+import net.kepano.database.DBAdapter;
+import net.kepano.database.DataHandler;
 import android.content.BroadcastReceiver;
+
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.util.Log;
 
 public class intentBroadcast extends BroadcastReceiver {
 	private final String STATE = "state";
 	private Intent i;
+	private DBAdapter database;
+
 	@Override
 	public void onReceive(Context context, Intent intent) {
-		Bundle b = intent.getExtras();
-		if (b.getInt(STATE) == 1){
-			Intent i; 
-			i = new Intent("com.android.music.musicservicecommand"); 
-			i.putExtra("command", "play");
-			context.sendBroadcast(i); 
-			
-			//i = new Intent(MediaStore.INTENT_ACTION_MUSIC_PLAYER);
-			//i.putExtra("command", "play");
-			//i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
-			//context.startActivity(i);
+		if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
+			database = new DBAdapter(context);
+			database.open();
+			Cursor c = database.getOption(DataHandler.OPTION_START_ON_BOOT);
+			c.moveToFirst();
+			if (c.getCount() == 1 && c.getString(0).equals("true")) {
+				context.startService(new Intent(context, detectJack.class));
+			}
+			database.close();
+		} else {
+			Bundle b = intent.getExtras();
+			if (b.getInt(STATE) == 1) {
+				Log.e("kepano", "head phones attached");
+				Intent i;
+				i = new Intent("com.android.music.musicservicecommand");
+				i.putExtra("command", "play");
+				context.getApplicationContext().sendBroadcast(i);
+				// context.sendBroadcast(i);
+			}
 		}
 	}
-	
 }
