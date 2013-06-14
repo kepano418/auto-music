2d51189b42d27bb9ebe6a1b8906390644d337bd4
jess.louderback@gmail.com <jess.louderback@gmail.com@7c3e6dd4-ce57-9d87-5feb-09a7e010751d>
2013-06-14 11:58:05 +0000

    uploaded to play store 1.2.3

--- AutoMusic/src/net/kepano/tools/tools.java
+++ AutoMusic/src/net/kepano/tools/tools.java
@@ -1,6 +1,7 @@
 package net.kepano.tools;
 
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.util.DisplayMetrics;
 
 public class tools {
@@ -9,4 +10,25 @@ public class tools {
 		return metrics.widthPixels;
 	}
 
+	public static boolean isVersionICSorBetter() {
+		int currentapiVersion = android.os.Build.VERSION.SDK_INT;
+
+		//if API is ICS
+		if (currentapiVersion >= 14) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+	
+	public static boolean isProInstalled(Context context) {
+	    PackageManager manager = context.getPackageManager();
+	    if (manager.checkSignatures(context.getPackageName(), "net.kepano.automusic.pro")
+	        == PackageManager.SIGNATURE_MATCH) {
+	        //Pro key installed, and signatures match
+	        return true;
+	    }
+	    return false;
+	}
 }
